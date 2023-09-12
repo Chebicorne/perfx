d0399792c33aa926c57c24dd178d9d6472a3c38a
Lenny ROLAND <thefunnygamers49@gmail.com>
2023-09-12 23:33:10 +0200

    change save workout

--- src/screens/home/WorkoutInProgress.tsx
+++ src/screens/home/WorkoutInProgress.tsx
@@ -3,6 +3,8 @@ import CustomText from '../../components/global/CustomText'
 import Swiper from 'react-native-swiper'
 import { useEffect, useState } from "react";
 import { Cell, TableWrapper } from "react-native-table-component";
+import { auth, db } from '../../firebase/firebaseconfig';
+import { collection, addDoc } from "firebase/firestore";
 
 type Exercise = {
     name: string;
@@ -12,22 +14,20 @@ type Exercise = {
     seriesCount: string;
 };
 
-
 const WorkoutInProgress = ({ route, navigation }: { route: any, navigation: any }) => {
     const { session } = route.params;
     const [exercises, setExercises] = useState<Exercise[]>([]);
-    const [tableBody, setTableBody] = useState<any[]>([]); // Contiendra les données du tableau
     const [viewWidth, setViewWidth] = useState<number>(0);
+    const [workoutData, setWorkoutData] = useState<any[]>([]);
 
     const percentages = [0.5, 0.13, 0.13, 0.14];  // 40%, 10%, 50%
     const widthArr = viewWidth ? percentages.map(percentage => viewWidth * percentage) : [];
     const tableHead = ['Série', 'Reps', 'Ressenti', "Note"];
+    const userId = auth.currentUser?.uid;
 
     useEffect(() => {
         if (session && session.exercises) {
             setExercises(session.exercises);
-            console.log(session.exercises);
-
         }
     }, [session]);
 
@@ -36,6 +36,35 @@ const WorkoutInProgress = ({ route, navigation }: { route: any, navigation: any 
         const sec = seconds % 60;
 
         return `${min} minutes ${sec} secondes`;
+    };
+
+    const handleRepsChange = (exerciseIndex: number, seriesIndex: number, reps: number) => {
+        const updatedWorkoutData = [...workoutData];
+        const currentExerciseName = exercises[exerciseIndex].name; // Supposons que "exercises" est votre tableau d'exercices
+
+        if (!updatedWorkoutData[exerciseIndex]) {
+            updatedWorkoutData[exerciseIndex] = { name: currentExerciseName, series: [] };
+        }
+        if (!updatedWorkoutData[exerciseIndex].series[seriesIndex]) {
+            updatedWorkoutData[exerciseIndex].series[seriesIndex] = { reps: 0, feeling: "", note: "" };
+        }
+        updatedWorkoutData[exerciseIndex].series[seriesIndex].reps = reps;
+        setWorkoutData(updatedWorkoutData);
+    };
+
+
+    const saveCompletedWorkout = async (workoutData: any) => {
+        try {
+            console.log(workoutData);
+
+            const docRef = await addDoc(collection(db, "doneWorkouts"), {
+                ...workoutData,
+                userId: userId
+            });
+            console.log("Workout saved successfully with ID: ", docRef.id);
+        } catch (error) {
+            console.error("Error saving workout: ", error);
+        }
     };
 
     return (
@@ -77,6 +106,7 @@ const WorkoutInProgress = ({ route, navigation }: { route: any, navigation: any 
                                             <TextInput
                                                 style={styles.input}
                                                 placeholderTextColor={"white"}
+                                                onChangeText={(text) => handleRepsChange(inputIndex, serieIndex, parseInt(text))}
                                             ></TextInput>
                                         }
                                         width={widthArr[inputIndex + 1]}
@@ -88,7 +118,8 @@ const WorkoutInProgress = ({ route, navigation }: { route: any, navigation: any 
                 ))}
 
             </Swiper>
-        </SafeAreaView>
+            <TouchableOpacity onPress={() => { saveCompletedWorkout(workoutData) }}><CustomText>Enregistrer</CustomText></TouchableOpacity >
+        </SafeAreaView >
     )
 }
 
